9792f73508577aa0e6fc0ffdd18c2f6f8d29fc2e
Sandra Milena Sotelo Tobar <sandyosef1617@gmail.com>
2024-08-11 01:45:21 -0500

    CRUD OK

--- api.js
+++ api.js
@@ -55,6 +55,8 @@ mongoose.connect(
   }
 );
 
+//CREAR TAREAS
+
 router.post("/create-task", function (req, res) {
   // el req es lo que llega (Cuando del lado del cliente http el hace una petición con cierta información esto llega al servidor y el servidor lo recibe y lo captura en esta variable req la cual tiene el body que es el cuerpo de la solicitud)
   // el res es la respuesta del lado del servidor, el servidor siempre debe responder, que hubo un error o que todo salio bien.
@@ -75,6 +77,48 @@ router.post("/create-task", function (req, res) {
     //Como este modelo ya implemento mongoose, el metodo save, lo que va a hacer es crear un nuevo recurso en la BD (nuevo registro), es decir el schema ya tiene toda esa información en la coleccion
     if (err) {
       console.log(err);
+      res.status(500).send("Internal error\n");
+    } else {
+      res.status(200).send("OK\n");
+    }
+  });
+});
+
+// CONSULTAR TAREAS
+
+router.get("/all-tasks", function (req, res) {
+  TaskModel.find(function (err, data) {
+    if (err) {
+      res.status(500).send("Internal error\n");
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//MODIFICAR UNA TAREA
+
+router.post("/update-task", function (req, res) {
+  TaskModel.updateOne(
+    { TaskId: req.body.TaskId }, //el primer argumento es el ID por el cual va a realizar la busqueda y el segundo es lo que le va a cambiar
+    {
+      Name: req.body.Name,
+      Deadline: req.body.Deadline,
+    },
+    function (err, data) {
+      if (err) {
+        res.status(500).send("Internal error\n");
+      } else {
+        res.status(200).send("OK\n");
+      }
+    }
+  );
+});
+
+//ELIMINAR UNA TAREA
+router.delete("/delete-task", function (req, res) {
+  TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+    if (err) {
       res.status(500).send("Internal error\n");
     } else {
       res.status(200).send("OK\n");
